11df604aad4ebcef113980eb8f47c90d9a28ac8c
Phúc <phucdh410@gmail.com>
2022-03-28 08:51:16 +0700

    Fix css icon

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,9 @@ import { ThemeProvider } from "@mui/material";
 import AOS from "aos";
 import config from "config";
 import Layout from "common/components/layout";
-// import ScrollTop from "common/components/other/ScrollTop";
 import Submenu from "common/components/layout/Submenu";
 import theme from "theme/theme";
+// import ScrollTop from "common/components/other/ScrollTop";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
@@ -26,8 +26,8 @@ function MyApp({ Component, pageProps }) {
         <Component {...pageProps} />
       </Layout>
 
-      {/* Button Scroll-top */}
-      {/* <ScrollTop /> */}
+      {/* Button Scroll-top
+      <ScrollTop /> */}
     </ThemeProvider>
   );
 }
